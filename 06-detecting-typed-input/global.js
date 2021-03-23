f43bd752c8f5b7c5463033ed3a7d99eee2edf436
Nikplay23 <78353018+Nikplay23@users.noreply.github.com>
2021-03-23 16:04:10 +0200

    Uzdevums

--- 06-detecting-typed-input/global.js
+++ 06-detecting-typed-input/global.js
@@ -1,1 +1,13 @@
 // Your JavaScript goes here.
+
+window.addEventListener("load", function(){
+
+    var name = document.getElementById("full_name")
+
+    name.addEventListener("keyup", function(){
+        var hello_andname = document.getElementById("greeting")
+
+        hello_andname.innerHTML = ("Hello there, " + name.value + "!")
+    });
+
+});
