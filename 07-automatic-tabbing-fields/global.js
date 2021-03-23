9e33cced349a9acc6e3760cbf0e71ab0437b501d
Nikplay23 <78353018+Nikplay23@users.noreply.github.com>
2021-03-23 16:29:22 +0200

    Uzdevums

--- 07-automatic-tabbing-fields/global.js
+++ 07-automatic-tabbing-fields/global.js
@@ -1,1 +1,27 @@
 // Your JavaScript goes here.
+window.onload = function() {
+
+    var first_numbers = document.getElementById("first");
+    var second_numbers = document.getElementById("second");
+    var third_numbers = document.getElementById("third");
+  
+    first_numbers.addEventListener("keyup", function(){
+      cursor = first_numbers.value.length;
+      number_next = parseInt(first_numbers.getAttribute("maxlength"));
+  
+      if (cursor === number_next) {
+        second.focus();
+      }
+    });
+  
+    second_numbers.addEventListener("keyup", function(){
+      cursor = second_numbers.value.length;
+      number_next = parseInt(second_numbers.getAttribute("maxlength"));
+  
+      if (cursor === number_next) {
+        third_numbers.focus();
+      }
+    });
+  
+  };
+    
